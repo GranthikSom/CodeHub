65b25afbf6b02ca288dec49e5dced982c7b23a96
Granthik Som <granthiksom@gmail.com>
2026-08-03 15:23:13 +0530

    github

---
+++ lib/dashboard/LandingPage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+class Landingpage extends StatelessWidget {
+  const Landingpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Ekhane amra main dashboard banabo',
+          style: TextStyle(fontSize: 40),
+          
+        ),
+      )
+    );
+  }
+}
